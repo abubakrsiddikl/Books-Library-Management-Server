54fd46b4bd258f48ff2ef0d3d280462445c44518
limon <limonsk026@gmail.com>
2025-06-21 00:04:01 +0600

    set env file

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
 import { Server } from "http";
 import app from "./app";
 import mongoose from "mongoose";
+import config from "./config";
 
 let server: Server;
-const PORT = 5000;
 
 async function main() {
   try {
-    await mongoose.connect("mongodb://localhost:27017/libraryManagementDB");
+    await mongoose.connect(config.database_url!);
     console.log("MongoDB connected successfull");
-    server = app.listen(PORT, () => {
-      console.log(`This server running on this port : ${PORT}`);
+    server = app.listen(config.port, () => {
+      console.log(`This server running on this port : ${config.port}`);
     });
   } catch (error) {
     console.log(error);
