63ffb0881aeb196d626c825d0bdee6c6503cee26
limon <limonsk026@gmail.com>
2025-06-21 22:13:49 +0600

    route update and some error solve

--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,8 @@ const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
-app.use("/books", booksRouter);
-app.use("/borrow", borrowRouter);
+app.use("/api/books", booksRouter);
+app.use("/api/borrow", borrowRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to the Library Management Server . ");
